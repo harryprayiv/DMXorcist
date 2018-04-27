0cb72bc4b9b4f4c8775eb7a19b5c00a3649503ea
harryprayiv <harryprayiv@gmail.com>
2018-04-27 12:51:44 -0400

    the DMX state machine

---
+++ src/statemachineDMX.hs
@@ -0,0 +1,36 @@
+-- DMX Finite State Machine
+-- repurposing a tutorial I found for FSM's
+-- https://wickstrom.tech/finite-state-machines/2017/11/10/finite-state-machines-part-1-modeling-with-haskell.html
+
+{-# LANGUAGE OverloadedStrings #-}
+module StateMachinesWithAdts where
+
+import           Control.Monad      (foldM)
+import           Data.List.NonEmpty
+import           Data.Text          (Text)
+import           Text.Printf        (printf)
+
+import           Checkout           (Card (..), CartItem (..), calculatePrice)
+import qualified PaymentProvider
+
+-- a data type to store current DMX state
+data DMXState
+  = NoItems
+  | HasItems (NonEmpty CartItem)
+  | NoCard (NonEmpty CartItem)
+  | CardSelected (NonEmpty CartItem)
+                 Card
+  | CardConfirmed (NonEmpty CartItem)
+                  Card
+  | OrderPlaced
+  deriving (Show, Eq)
+
+-- will become a send DMX event
+data SendDMXEvent
+  = Select CartItem
+  | Checkout
+  | SelectCard Card
+  | Confirm
+  | PlaceOrder
+  | Cancel
+  deriving (Show, Eq)
